93133552845edec2ada1584fe27d8f6f1d75136b
Amir Farahani <najafabadi.a.h@gmail.com>
2022-09-07 23:51:11 +0200

    schedule a job

--- schedulerApp/views.py
+++ schedulerApp/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Request
 from .serializers import RequestSerializer
+from .task import reverse_request_title
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 
 class SchedulerApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -17,12 +20,19 @@ class SchedulerApiView(APIView):
     # schedule new request
     def post(self, request):
         data = {
-            'description': request.data.get('description'),
+            'title': request.data.get('title'),
             'scheduledDateTime': request.data.get('scheduledDateTime'),
             'status': 'pending'
         }
         serializer = RequestSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+        if data['scheduledDateTime'] >= datetime.now().strftime('%Y-%m-%d %H:%M:%S'): 
+            if serializer.is_valid():
+                serializer.save()
+                try:
+                    scheduler = BackgroundScheduler()
+                    scheduler.add_job(reverse_request_title, 'date', run_date=data['scheduledDateTime'], args=[serializer.data['id']], name=str(serializer.data['id']))
+                    scheduler.start()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                except:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"You must insert a datetime in the future"}, status=status.HTTP_400_BAD_REQUEST) 
